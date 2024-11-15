30870e95dd0bb0f3ebdfa995accb85178477e4a4
trishacapitle <140716826+trishacapitle@users.noreply.github.com>
2024-11-15 22:08:47 +0800

    added sidebar functionality

--- js/main-page.js
+++ js/main-page.js
@@ -1,1 +1,15 @@
-const menu
+const menu = document.getElementById('menu');
+const menuClose = document.getElementById('menu-close');
+const sideBar = document.querySelector('.navbar-mobile');
+
+
+function showSidebar() {
+    sideBar.style.display = 'flex';
+}
+
+function hideSidebar() {
+    sideBar.style.display = 'none';
+}
+
+menu.addEventListener('click', showSidebar);
+menuClose.addEventListener('click', hideSidebar);
