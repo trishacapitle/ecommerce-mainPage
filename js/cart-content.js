114953e1e8879083f4aa8f903dacb18ed808e6ca
trishacapitle <140716826+trishacapitle@users.noreply.github.com>
2024-11-28 01:31:57 +0800

    cart count functionality with qty

---
+++ js/cart-content.js
@@ -0,0 +1,5 @@
+const qtyCount = document.querySelector('.qty-count');
+
+const cartCount = document.querySelector('.cart-count');
+const cartContent = document.querySelector('.cart-content');
+
--- js/qty-handler.js
+++ js/qty-handler.js
@@ -1,13 +1,34 @@
 const minusBtn = document.getElementById('minus-btn');
 const addBtn = document.getElementById('add-btn');
 const qtyCount = document.querySelector('.qty-count');
+const cartCount = document.querySelector('.cart-count');
+const cartBtn = document.getElementById('add-cart-btn');
 
-let qtyStore = JSON.parse(localStorage.getItem('qty')) || 0;
-qtyCount.textContent = qtyStore;
+let qty = parseInt(localStorage.getItem('qty')) || 0;
+let cartQty = parseInt(localStorage.getItem('cartQty')) || 0;
+
+qtyCount.textContent = qty;
+cartCount.textContent = cartQty;
 
 minusBtn.addEventListener('click', () => {
-    qtyStore = localStorage.setItem('qty', qtyCount.textContent--);
+  if (qty > 0) {
+    qty--;
+    localStorage.setItem('qty', qty);
+    qtyCount.textContent = qty;
+  }
 });
+
 addBtn.addEventListener('click', () => {
-    qtyStore = localStorage.setItem('qty', qtyCount.textContent++);
+  qty++;
+  localStorage.setItem('qty', qty);
+  qtyCount.textContent = qty;
+});
+
+cartBtn.addEventListener('click', () => {
+  cartQty += qty;
+  localStorage.setItem('cartQty', cartQty);
+  cartCount.textContent = cartQty;
+  qty = 0;
+  localStorage.setItem('qty', qty);
+  qtyCount.textContent = qty;
 });
